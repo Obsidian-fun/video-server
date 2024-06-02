712a3cade96c5bcdc15dbdef3c5b0770946d8899
Obsidian <obsidiancrystal@tutanota.com>
2024-06-03 02:32:19 +0530

    modified:   main.go

--- main.go
+++ main.go
@@ -5,14 +5,23 @@ import (
 	"fmt"
 	"net/http"
 	"log"
-
 	"github.com/gorilla/mux"
 )
 
 func serveVideo(w http.ResponseWriter, r *http.Request) {
-	
+	fmt.Fprintf(w, "Serving video...");
+	video ,err := http.Dir("./videos/").Open("Secret.mp4");
+	if err != nil {
+		http.Error(w, "Video not found",http.StatusNotFound);
+		return
+	}
 
+	// headers,
+	w.Header().Set("Content-Type","video/mp4");
+	w.Header().Set("Content-Disposition","attachment; filename=Secret.mp4");
 
+	defer video.Close();
+	http.ServeContent(w, r, "Secret.mp4", video.Stat().Modtime(), video);
 }
 
 func main() {
