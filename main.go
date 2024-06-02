cca52a318a947dbf395891c5c2a3ac3002cdc362
Obsidian <obsidiancrystal@tutanota.com>
2024-06-03 02:00:42 +0530

    modified:   main.go 	deleted:    sample.mp3 	new file:   videos/Secret.mp4

--- main.go
+++ main.go
@@ -10,19 +10,8 @@ import (
 )
 
 func serveVideo(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello there Friend\n\n");
+	
 
-	cookie := &http.Cookie{
-		Name:"Example.com",
-		Value:"1234",
-		Domain:"happygolucky.com",
-	}
-	http.SetCookie(w, cookie);
-
-	fmt.Fprintf(w, fmt.Sprintf("Name:%s,\nDomain:%s\n", cookie.Name, cookie.Domain));
-
-	ctx := r.Context();
-	fmt.Fprintf(w, ctx);
 
 }
 
